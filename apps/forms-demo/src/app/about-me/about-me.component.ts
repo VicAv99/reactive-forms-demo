60b5eff395c5ef2c8a629210c8e125d49b7ac2c6
Victor Avila <vicav99@gmail.com>
2021-09-14 11:05:44 -0700

    feat: finish about me component (training)

--- apps/forms-demo/src/app/about-me/about-me.component.ts
+++ apps/forms-demo/src/app/about-me/about-me.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'workspace-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.scss']
+  styleUrls: ['./about-me.component.scss'],
 })
 export class AboutMeComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  submit() {
+    alert(JSON.stringify(this.form.value, null, 2));
+  }
+
+  reset() {
+    this.form.reset();
+  }
+
+  private initForm() {
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      aboutMe: '',
+      devStack: [''],
+    });
+  }
 }
